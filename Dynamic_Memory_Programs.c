65e98788d6312965d3c0d0b52a9f1fb1524510e7
golisrikanth1989 <golisrikanth1989@gmail.com>
2023-03-28 12:43:10 +0530

    malloc and calloc comparsion

--- Dynamic_Memory_Programs.c
+++ Dynamic_Memory_Programs.c
@@ -1,28 +1,57 @@
 #include<stdio.h>
 #include<stdlib.h>
 
+// int main()
+// {
+//     int i,n;
+//     printf("Enter the number of digits to store: \n");
+//     scanf("%d",&n);
+//     int *ptr = (int *)malloc(n*sizeof(int));
+
+//     if (ptr == NULL)
+//     {
+//        printf("Memory is not Available and allocated\n");
+//        exit(1);
+//     }
+
+//     for(i=0;i<n;i++)
+//     {
+//         printf("%d\n",*(ptr+i));
+//     } 
+
+//     for(i=0;i<n;i++)
+//     {
+//         printf("Enter %d element\n",i);
+//         scanf("%d",ptr+i);
+//     }
+
+//     for(i=0;i<n;i++)
+//     {
+//         printf("%d\n",*(ptr+i));
+//     }     
+
+// MALLOC and FREE
+int *input()
+{
+    int *ptr, i;
+    ptr = (int*)malloc(5*sizeof(int));
+    printf("Enter 5 numbers:\n");
+    for(i=0;i<5;i++)
+    {
+        scanf("%d",ptr+i);
+    } 
+    return ptr;
+}
 int main()
 {
-    int i,n;
-    printf("Enter the number of digits to store: \n");
-    scanf("%d",&n);
-    int *ptr = (int *)malloc(n*sizeof(int));
-
-    if (ptr == NULL)
-    {
-       printf("Memory is not Available and allocated\n");
-       exit(1);
-    }
-
-    for(i=0;i<n;i++)
-    {
-        printf("Enter %d element\n",i);
-        scanf("%d",ptr+i);
-    }
-
-    for(i=0;i<n;i++)
-    {
-        printf("%d\n",*(ptr+i));
-    }     
-
+  int i, sum=0;
+  int *ptr = input();
+  for(i=0;i<5;i++)
+  {
+    sum +=*(ptr+i);
+  }
+  printf("Sum is %d",sum);
+  free(ptr);
+  ptr = NULL;
+  return 0;
 }
